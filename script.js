16a6e698db1812bff28308f9d086c8a29835fd95
ahad <ahadrahman6@gmail.com>
2020-09-04 01:00:18 +0800

    implmented the add todo functionality

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ var todoInput = document.querySelector("#todo-text");
 var todoForm = document.querySelector("#todo-form");
 var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
-
+var errorMessage = document.getElementById('error');
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
 
 function renderToDos() {
     todoList.innerHTML = "";
+    errorMessage.innerText = "";
     todoCountSpan.textContent = todos.length;
     for (var i = 0; i < todos.length; i++) {
         var li = document.createElement("li");
@@ -15,6 +16,32 @@ function renderToDos() {
 
     }
 }
+// listen for when the form is submitted or user preses enter key
+todoForm.addEventListener('submit', function (event) {
+    // stop the browser from refreshing or submitting
+    event.preventDefault();
+    // get the value of the input and remove spaces before and after
+    var todo = todoInput.value.trim();
+    // check if the todo is empty
+    // if empty stop excecution of this code and display error message
+    if (todo === "") {
+        errorMessage.innerText = "Please type something";
+        return
+    }
+    // check if the same todo exists
+    if (todos.includes(todo)) {
+        errorMessage.innerText = "That todo already exists";
+        return
+    }
+    todos.push(todo);
+    todoInput.value = "";
+    renderToDos();
+
+})
+
+
+
+
 
 
 
